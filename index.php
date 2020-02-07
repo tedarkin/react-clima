d442e099d5f5e2f63b4a10201fe782502bf26f38
Nico T <nicolas.e.tastaca@gmail.com>
2020-02-07 14:07:54 +0100

    zaz

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CLIMA REACT</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/sketchy/bootstrap.min.css">
+<link href="bundle.css" rel="stylesheet"></head>
+<body>
+
+    <!-- no necesito importar el js porque webpack ya sabe -->
+    <div id="app"></div>
+    
+<script type="text/javascript" src="bundle.js"></script></body>
+</html>
